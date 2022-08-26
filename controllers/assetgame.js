6a4ab7156ad7c178c818ccc469dc80653cf679a4
Malte0621 <malte.kihlstenius@gmail.com>
2022-08-26 20:26:50 +0200

    Avatar (COLORS) update.
    Removed the old bacon avatar and allowed users to customize their bodycolors. THIS DOES NOT INCLUDE ACCESSORIES YET.

--- controllers/assetgame.js
+++ controllers/assetgame.js
@@ -439,27 +439,32 @@ module.exports = {
         });
 
         app.get("/Asset/CharacterFetch.ashx", (req, res) => {
-            res.send("https://www.rbx2016.tk/asset/BodyColors.ashx?userId=1;http://www.rbx2016.tk/asset/?id=63690008&version=5;http://www.rbx2016.tk/asset/?id=144076358;http://www.rbx2016.tk/asset/?id=144076760;http://www.rbx2016.tk/asset/?id=144075659;http://www.rbx2016.tk/asset/?id=86500054&version=1;http://www.rbx2016.tk/asset/?id=86500078&version=1;http://www.rbx2016.tk/asset/?id=86500036&version=1;http://www.rbx2016.tk/asset/?id=86500008&version=1;http://www.rbx2016.tk/asset/?id=86500064&version=1;http://www.rbx2016.tk/asset/?id=86498048&version=1")
+            const userId = parseInt(req.query.player);
+            res.send(`https://www.rbx2016.tk/asset/BodyColors.ashx?userId=${userId}`) // ;http://www.rbx2016.tk/asset/?id=63690008&version=5;http://www.rbx2016.tk/asset/?id=144076358;http://www.rbx2016.tk/asset/?id=144076760;http://www.rbx2016.tk/asset/?id=144075659;http://www.rbx2016.tk/asset/?id=86500054&version=1;http://www.rbx2016.tk/asset/?id=86500078&version=1;http://www.rbx2016.tk/asset/?id=86500036&version=1;http://www.rbx2016.tk/asset/?id=86500008&version=1;http://www.rbx2016.tk/asset/?id=86500064&version=1;http://www.rbx2016.tk/asset/?id=86498048&version=1
         });
 
         app.get("/Assets/CharacterFetch.php", (req, res) => {
-            res.send("https://www.rbx2016.tk/asset/BodyColors.ashx?userId=1;http://www.rbx2016.tk/asset/?id=63690008&version=5;http://www.rbx2016.tk/asset/?id=144076358;http://www.rbx2016.tk/asset/?id=144076760;http://www.rbx2016.tk/asset/?id=144075659;http://www.rbx2016.tk/asset/?id=86500054&version=1;http://www.rbx2016.tk/asset/?id=86500078&version=1;http://www.rbx2016.tk/asset/?id=86500036&version=1;http://www.rbx2016.tk/asset/?id=86500008&version=1;http://www.rbx2016.tk/asset/?id=86500064&version=1;http://www.rbx2016.tk/asset/?id=86498048&version=1")
+            const userId = parseInt(req.query.player);
+            res.send(`https://www.rbx2016.tk/asset/BodyColors.ashx?userId=${userId}`); // ;http://www.rbx2016.tk/asset/?id=63690008&version=5;http://www.rbx2016.tk/asset/?id=144076358;http://www.rbx2016.tk/asset/?id=144076760;http://www.rbx2016.tk/asset/?id=144075659;http://www.rbx2016.tk/asset/?id=86500054&version=1;http://www.rbx2016.tk/asset/?id=86500078&version=1;http://www.rbx2016.tk/asset/?id=86500036&version=1;http://www.rbx2016.tk/asset/?id=86500008&version=1;http://www.rbx2016.tk/asset/?id=86500064&version=1;http://www.rbx2016.tk/asset/?id=86498048&version=1
         });
 
-        app.get("/asset/BodyColors.ashx", (req, res) => {
+        app.get("/asset/BodyColors.ashx", async (req, res) => {
+            const userId = parseInt(req.query.userId);
+            const avatarColors = await db.getUserProperty(userId, "avatarColors") || [1002, 1002, 1002, 1002, 1002, 1002];
+            
             res.send(`<?xml version="1.0" encoding="utf-8" ?>
             <roblox xmlns:xmime="http://www.w3.org/2005/05/xmlmime" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://www.rbx2016.tk/roblox.xsd" version="4">
                 <External>null</External>
                 <External>nil</External>
                 <Item class="BodyColors">
                     <Properties>
-                        <int name="HeadColor">1002</int>
-                        <int name="LeftArmColor">1002</int>
-                        <int name="LeftLegColor">1002</int>
+                        <int name="HeadColor">${avatarColors[0]}</int>
+                        <int name="LeftArmColor">${avatarColors[1]}</int>
+                        <int name="LeftLegColor">${avatarColors[2]}</int>
                         <string name="Name">Body Colors</string>
-                        <int name="RightArmColor">1002</int>
-                        <int name="RightLegColor">1002</int>
-                        <int name="TorsoColor">1002</int>
+                        <int name="RightArmColor">${avatarColors[3]}</int>
+                        <int name="RightLegColor">${avatarColors[4]}</int>
+                        <int name="TorsoColor">${avatarColors[5]}</int>
                         <bool name="archivable">true</bool>
                     </Properties>
                 </Item>
